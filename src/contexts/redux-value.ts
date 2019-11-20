bb1207cce8311c1d0826c4e6e7f3e92a53630431
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-11-20 23:19:21 +0100

    feat: getReduxContextValue function added

--- src/contexts/redux-value.ts
+++ src/contexts/redux-value.ts
@@ -1,5 +1,7 @@
+import { initialUserState, UserReducer } from "./../store/user-store";
 import { UserService } from "./../services/user-service";
 import { UserStateType } from "../store/user-store";
+import { useReducer } from "react";
 
 export type ReduxValueType = {
   store: {
@@ -8,4 +10,16 @@ export type ReduxValueType = {
   services: {
     userService: UserService;
   };
+};
+
+export const getReduxContextValue = (): ReduxValueType => {
+  const [userState, userDispatch] = useReducer(UserReducer, initialUserState);
+  const userService = new UserService(userDispatch);
+
+  const value: ReduxValueType = {
+    store: { userState },
+    services: { userService }
+  };
+
+  return value;
 };
