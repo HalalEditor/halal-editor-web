a4a1ee55f035f3920441a0310ad5e6cd69edd8d9
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-11-20 23:19:56 +0100

    feat: redux-context created

---
+++ src/contexts/redux-context.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, ReactChild } from "react";
+import { ReduxValueType, getReduxContextValue } from "./redux-value";
+
+export const ReduxContext = createContext<ReduxValueType>({} as ReduxValueType);
+export const useReduxContextValue = () => useContext(ReduxContext);
+
+type Props = {
+  children: ReactChild;
+};
+
+const ReduxContextProvider = (props: Props) => {
+  const appReduxValue = getReduxContextValue();
+
+  return (
+    <ReduxContext.Provider value={appReduxValue}>
+      {props.children}
+    </ReduxContext.Provider>
+  );
+};
+
+export default ReduxContextProvider;
