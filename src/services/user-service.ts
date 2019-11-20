645330b3e557e4955cf8b04acff2fd98f87b6e56
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-11-21 00:56:01 +0100

    feat: signInWithEmailAndPassword and createUserWithEmailAndPassword functions created

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,6 +1,26 @@
 import { UserActionType } from "./../store/user-store";
 import { Dispatch } from "react";
 
+import app from "firebase";
+
 export class UserService {
   constructor(private dispatch: Dispatch<UserActionType>) {}
+
+  signInWithEmailAndPassword(email: string, password: string) {
+    app
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(res => {
+        console.log(`signInWithEmailAndPassword result ${res}`);
+      });
+  }
+
+  createUserWithEmailAndPassword(email: string, password: string) {
+    app
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(res => {
+        console.log(`createUserWithEmailAndPassword result ${res}`);
+      });
+  }
 }
