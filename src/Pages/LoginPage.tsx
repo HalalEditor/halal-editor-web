bb86d91823495f9dc6915793605504c7dc610bea
cangir <ahmetcangir07@gmail.com>
2019-11-20 22:37:04 +0100

    Add Login Page

---
+++ src/Pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import MainBar from "../Components/MainBar";
+
+const LoginPage: FC = () => {
+  return (
+    <div>
+      <MainBar></MainBar>
+      Home Page
+    </div>
+  );
+};
+
+export default LoginPage;
