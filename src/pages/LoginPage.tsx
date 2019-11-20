0e8d74cfb2cf93ca2e1ea2e7c9b05dddaafde54e
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-11-21 00:59:11 +0100

    feat: loginFunction implemented to LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,34 +1,62 @@
-import React, { FC } from "react";
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import React, { FC, useState } from "react";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment
+} from "semantic-ui-react";
+import { useReduxContextValue } from "../contexts/redux-context";
 
 const LoginPage: FC = () => {
-  return (
-    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as='h2' color='teal' textAlign='center'>
-        Log in to your account
-      </Header>
-      <Form size='large'>
-        <Segment stacked>
-          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-          <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            type='password'
-          />
+  const { userService } = useReduxContextValue().services;
 
-          <Button color='teal' fluid size='large'>
-            Login
-          </Button>
-        </Segment>
-      </Form>
-      <Message>
-        New to us? <a href='#'>Sign Up</a>
-      </Message>
-    </Grid.Column>
-  </Grid>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginButtonHandle = () => {
+    userService.signInWithEmailAndPassword(email, password);
+  };
+
+  return (
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="teal" textAlign="center">
+          Log in to your account
+        </Header>
+        <Form size="large">
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="E-mail address"
+              onChange={event => {
+                setEmail(event.target.value);
+              }}
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+              onChange={event => {
+                setPassword(event.target.value);
+              }}
+            />
+
+            <Button color="teal" fluid size="large" onClick={loginButtonHandle}>
+              Login
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          New to us? <a href="#">Sign Up</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
   );
 };
 
