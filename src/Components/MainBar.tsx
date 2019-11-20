4368b68898b1af26edcd5e735780d06198c2d5c3
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-11-20 22:03:38 +0100

    first commit

---
+++ src/Components/MainBar.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const MainBar: FC = () => {
+  return <div> Main Bar </div>;
+};
+
+export default MainBar;
---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import MainBar from "../Components/MainBar";
+
+const HomePage: FC = () => {
+  return (
+    <div>
+      <MainBar></MainBar>
+      Home Page
+    </div>
+  );
+};
+
+export default HomePage;
