12004d60b5e59ba6fa8a3ec240be17d3380022e8
cangir <ahmetcangir07@gmail.com>
2019-11-21 18:05:17 +0100

    feat: add dashboard page

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import MainBar from "../components/MainBar";
+
+const DashboardPage: FC = () => {
+  return (
+    <div>
+      <MainBar></MainBar>
+      Dashboard Page
+    </div>
+  );
+};
+
+export default DashboardPage;
