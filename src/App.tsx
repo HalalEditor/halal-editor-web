9c099ec590fdc076f509634aa9de3b26b8c7c00f
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-11-21 19:57:27 +0100

    feat: routing moved to router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import app from "firebase/app";
 
 import "./App.css";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import DashboardPage from "./pages/DashboardPage";
 import { FIREBASE_CONFIG } from "./.config";
-import ReduxContextProvider, { ReduxContext } from "./contexts/redux-context";
+import ReduxContextProvider from "./contexts/redux-context";
+import Router from "./Router";
 
 const App: React.FC = () => {
   useEffect(() => {
@@ -15,31 +12,9 @@ const App: React.FC = () => {
     app.initializeApp(FIREBASE_CONFIG);
   }, []);
 
-  const isAuth = false;
-  let pages = null;
-
-  if (isAuth) {
-    pages = (
-      <div className="page-container">
-        <Switch>
-          <Route path="/" exact component={DashboardPage} />
-        </Switch>
-      </div>
-    );
-  } else {
-    pages = (
-      <div className="page-container">
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/login" exact component={LoginPage} />
-        </Switch>
-      </div>
-    );
-  }
-
   return (
     <ReduxContextProvider>
-      <Router>{pages}</Router>
+      <Router></Router>
     </ReduxContextProvider>
   );
 };
