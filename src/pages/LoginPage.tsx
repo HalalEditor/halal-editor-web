d8d0246f7f96a85657136bf81e4af7169ecd3862
cangir <ahmetcangir07@gmail.com>
2019-11-22 00:05:45 +0100

    refactor: implement tabler-react login form and login with mail

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,7 @@
 import React, { FC, useState } from "react";
+import { Button, Form, Grid, Header, Page, Text } from "tabler-react";
+import logo from "../assets/logo.svg";
+import "../styles.scss";
 
 import { useReduxContextValue } from "../contexts/redux-context";
 import { useHistory } from "react-router";
@@ -12,12 +15,61 @@ const LoginPage: FC = () => {
   const loginButtonHandle = async () => {
     const result = await userService.signInWithEmailAndPassword(email, password);
     console.log(result);
-    if (result == undefined) {
+    if (result === undefined) {
       history.replace("/");
     }
   };
 
-  return <div>Login</div>;
+  return (
+    <Page className="page-single middle">
+      <div className="container">
+        <div className="row">
+          <Grid.Col className="col-login mx-auto">
+            <div className="text-center">
+              <img alt="Halal Editor" src={logo} className="h-8" />
+              <Header.H1 className="text-success mb-0 mt-4">Halal Editor</Header.H1>
+            </div>
+            <Form className="card" autoComplete="off">
+              <div className="card-body p-6">
+                <Button block social="google" className="text-left mb-6">
+                  Sign in with Google
+                </Button>
+                <div className="or-seperator">
+                  <i className="text-muted">or</i>
+                </div>
+                <Form.Group label="User Name">
+                  <Form.Input
+                    name="example-text-input"
+                    placeholder="E-mail address..."
+                    onChange={event => {
+                      setEmail(event.target.value);
+                    }}
+                  />
+                </Form.Group>
+                <Form.Group label="Password">
+                  <Form.Input
+                    name="example-text-input"
+                    placeholder="Password..."
+                    onChange={event => {
+                      setPassword(event.target.value);
+                    }}
+                  />
+                </Form.Group>
+                <Button block type="submit" color="primary" onClick={loginButtonHandle}>
+                  Login
+                </Button>
+              </div>
+            </Form>
+            <div className="text-center">
+              <Text className="text-center text-muted">
+                Not a member? <a href="/">Sign up now</a>
+              </Text>
+            </div>
+          </Grid.Col>
+        </div>
+      </div>
+    </Page>
+  );
 };
 
 export default LoginPage;
