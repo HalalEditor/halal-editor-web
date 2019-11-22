4a576cf843059b472fb0cbd98168736e40556742
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-11-22 22:16:21 +0100

    refactor: detect bug on Page  tabler-react component
    page auto redirect

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,75 +4,102 @@ import logo from "../assets/logo.svg";
 import "../styles.scss";
 
 import { useReduxContextValue } from "../contexts/redux-context";
-import { useHistory, Redirect } from "react-router";
+import { Redirect } from "react-router";
 
 const LoginPage: FC = () => {
   const { services, store } = useReduxContextValue();
-  let history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const loginButtonHandle = async () => {
+  const unSubmitLoginFormHandle = async () => {
+    console.log("loging....");
     const result = await services.userService.signInWithEmailAndPassword(email, password);
-    console.log(result);
-
-    if (result === undefined) {
-      history.replace("/");
-    }
+    console.log("loginButtonHandle:", result);
   };
 
   return store.userState.isAuth ? (
     <Redirect to="/"></Redirect>
   ) : (
-    <Page className="page-single middle">
-      <div className="container">
-        <div className="row">
-          <Grid.Col className="col-login mx-auto">
-            <div className="text-center">
-              <img alt="Halal Editor" src={logo} className="h-8" />
-              <Header.H1 className="text-success mb-0 mt-4">Halal Editor</Header.H1>
-            </div>
-            <Form className="card" autoComplete="off" onSubmit={event => console.log("clicked")}>
-              <div className="card-body p-6">
-                <Button block social="google" className="text-left mb-6">
-                  Sign in with Google
-                </Button>
-                <div className="or-seperator">
-                  <i className="text-muted">or</i>
-                </div>
-                <Form.Group label="User Name">
-                  <Form.Input
-                    icon="user"
-                    placeholder="E-mail address..."
-                    onChange={event => {
-                      setEmail(event.target.value);
-                    }}
-                  />
-                </Form.Group>
-                <Form.Group label="Password">
-                  <Form.Input
-                    type="password"
-                    icon="lock"
-                    placeholder="Password..."
-                    onChange={event => {
-                      setPassword(event.target.value);
-                    }}
-                  />
-                </Form.Group>
-                <Button block color="primary" onClick={loginButtonHandle}>
-                  Login
-                </Button>
-              </div>
-            </Form>
-            <div className="text-center">
-              <Text className="text-center text-muted">
-                Not a member? <a href="/">Sign up now</a>
-              </Text>
-            </div>
-          </Grid.Col>
-        </div>
-      </div>
-    </Page>
+    <div className="card-body p-6">
+      <Form.Group label="User Name">
+        <Form.Input
+          icon="user"
+          placeholder="E-mail address..."
+          onChange={event => {
+            setEmail(event.target.value);
+          }}
+        />
+      </Form.Group>
+      <Form.Group label="Password">
+        <Form.Input
+          type="password"
+          icon="lock"
+          placeholder="Password..."
+          onChange={event => {
+            setPassword(event.target.value);
+          }}
+        />
+      </Form.Group>
+      <Button block color="primary" onClick={unSubmitLoginFormHandle}>
+        Login
+      </Button>
+    </div>
+    // <Page className="page-single middle">
+    //   <div className="container">
+    //     <div className="row">
+    //       <Grid.Col className="col-login mx-auto">
+    //         <div className="text-center">
+    //           <img alt="Halal Editor" src={logo} className="h-8" />
+    //           <Header.H1 className="text-success mb-0 mt-4">Halal Editor</Header.H1>
+    //         </div>
+    //         <Button block social="google" className="text-left mb-6">
+    //           Sign in with Google
+    //         </Button>
+    //         <div className="or-seperator">
+    //           <i className="text-muted">or</i>
+    //         </div>
+    //         <Form
+    //           className="card"
+    //           autoComplete="off"
+    //           onSubmit={() => {
+    //             console.log("form submitting");
+    //             // unSubmitLoginFormHandle();
+    //           }}
+    //         >
+    //           <div className="card-body p-6">
+    //             <Form.Group label="User Name">
+    //               <Form.Input
+    //                 icon="user"
+    //                 placeholder="E-mail address..."
+    //                 onChange={event => {
+    //                   setEmail(event.target.value);
+    //                 }}
+    //               />
+    //             </Form.Group>
+    //             <Form.Group label="Password">
+    //               <Form.Input
+    //                 type="password"
+    //                 icon="lock"
+    //                 placeholder="Password..."
+    //                 onChange={event => {
+    //                   setPassword(event.target.value);
+    //                 }}
+    //               />
+    //             </Form.Group>
+    //             <Button block color="primary" type="submit">
+    //               Login
+    //             </Button>
+    //           </div>
+    //         </Form>
+    //         {/* <div className="text-center">
+    //           <Text className="text-center text-muted">
+    //             Not a member? <a href="/">Sign up now</a>
+    //           </Text>
+    //         </div> */}
+    //       </Grid.Col>
+    //     </div>
+    //   </div>
+    // </Page>
   );
 };
 
