d64c7da23198df9fd654e881e8dd159103563eb0
cangir <ahmetcangir07@gmail.com>
2019-11-22 02:30:41 +0100

    feat: add Profile page

---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import { Header } from "tabler-react";
+import SiteWrapper from "../SiteWrapper";
+
+const ProfilePage: FC = () => {
+  return (
+    <SiteWrapper>
+      <Header.H1>Profile</Header.H1>
+    </SiteWrapper>
+  );
+};
+
+export default ProfilePage;
