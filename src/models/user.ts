3ed2db7a6d8ce40a945d7831b6d527508486c292
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-11-22 20:50:38 +0100

    feat: usercategory propery can be undefined

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,7 +2,7 @@ export type User = {
   _id: string;
   email: string;
   emailVerified: boolean;
-  userCategory: UserCategory;
+  userCategory?: UserCategory;
 };
 
 export type UserCategory = "root" | "admin" | "editor" | "normal";
