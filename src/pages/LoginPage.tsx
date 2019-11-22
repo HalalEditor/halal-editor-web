3c272c15366bcf0d8304da07311e1fed5a136c33
cangir <ahmetcangir07@gmail.com>
2019-11-22 23:08:15 +0100

    refactor: fix styled form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -20,86 +20,54 @@ const LoginPage: FC = () => {
   return store.userState.isAuth ? (
     <Redirect to="/"></Redirect>
   ) : (
-    <div className="card-body p-6">
-      <Form.Group label="User Name">
-        <Form.Input
-          icon="user"
-          placeholder="E-mail address..."
-          onChange={event => {
-            setEmail(event.target.value);
-          }}
-        />
-      </Form.Group>
-      <Form.Group label="Password">
-        <Form.Input
-          type="password"
-          icon="lock"
-          placeholder="Password..."
-          onChange={event => {
-            setPassword(event.target.value);
-          }}
-        />
-      </Form.Group>
-      <Button block color="primary" onClick={unSubmitLoginFormHandle}>
-        Login
-      </Button>
-    </div>
-    // <Page className="page-single middle">
-    //   <div className="container">
-    //     <div className="row">
-    //       <Grid.Col className="col-login mx-auto">
-    //         <div className="text-center">
-    //           <img alt="Halal Editor" src={logo} className="h-8" />
-    //           <Header.H1 className="text-success mb-0 mt-4">Halal Editor</Header.H1>
-    //         </div>
-    //         <Button block social="google" className="text-left mb-6">
-    //           Sign in with Google
-    //         </Button>
-    //         <div className="or-seperator">
-    //           <i className="text-muted">or</i>
-    //         </div>
-    //         <Form
-    //           className="card"
-    //           autoComplete="off"
-    //           onSubmit={() => {
-    //             console.log("form submitting");
-    //             // unSubmitLoginFormHandle();
-    //           }}
-    //         >
-    //           <div className="card-body p-6">
-    //             <Form.Group label="User Name">
-    //               <Form.Input
-    //                 icon="user"
-    //                 placeholder="E-mail address..."
-    //                 onChange={event => {
-    //                   setEmail(event.target.value);
-    //                 }}
-    //               />
-    //             </Form.Group>
-    //             <Form.Group label="Password">
-    //               <Form.Input
-    //                 type="password"
-    //                 icon="lock"
-    //                 placeholder="Password..."
-    //                 onChange={event => {
-    //                   setPassword(event.target.value);
-    //                 }}
-    //               />
-    //             </Form.Group>
-    //             <Button block color="primary" type="submit">
-    //               Login
-    //             </Button>
-    //           </div>
-    //         </Form>
-    //         {/* <div className="text-center">
-    //           <Text className="text-center text-muted">
-    //             Not a member? <a href="/">Sign up now</a>
-    //           </Text>
-    //         </div> */}
-    //       </Grid.Col>
-    //     </div>
-    //   </div>
-    // </Page>
+    <Page className="page-single middle">
+      <div className="container">
+        <div className="row">
+          <Grid.Col className="col-login mx-auto">
+            <div className="text-center">
+              <img alt="Halal Editor" src={logo} className="h-8" />
+              <Header.H1 className="text-success mb-0 mt-4">Halal Editor</Header.H1>
+            </div>
+
+            <div className="card-body p-6">
+              <Button block social="google" className="text-left mb-6">
+                Sign in with Google
+              </Button>
+              <div className="or-seperator">
+                <i className="text-muted">or</i>
+              </div>
+              <Form.Group label="User Name">
+                <Form.Input
+                  icon="user"
+                  placeholder="E-mail address..."
+                  onChange={event => {
+                    setEmail(event.target.value);
+                  }}
+                />
+              </Form.Group>
+              <Form.Group label="Password">
+                <Form.Input
+                  type="password"
+                  icon="lock"
+                  placeholder="Password..."
+                  onChange={event => {
+                    setPassword(event.target.value);
+                  }}
+                />
+              </Form.Group>
+              <Button block color="primary" onClick={unSubmitLoginFormHandle}>
+                Login
+              </Button>
+            </div>
+            <div className="text-center">
+              <Text className="text-center text-muted">
+                Not a member? <a href="/">Sign up now</a>
+              </Text>
+            </div>
+          </Grid.Col>
+        </div>
+      </div>
+    </Page>
   );
 };
 
