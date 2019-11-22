4318d52348a812fd3f7bebc5c58a91b87dce43fd
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-11-22 22:13:21 +0100

    feat: implement account menu

--- src/layout/SiteWrapper.tsx
+++ src/layout/SiteWrapper.tsx
@@ -1,9 +1,10 @@
-import * as React from "react";
+import React from "react";
 import { NavLink, withRouter } from "react-router-dom";
-import logo from "./assets/logo-text.svg";
-import userAvatar from "./assets/user.svg";
+import logo from "../assets/logo-text.svg";
 
-import { Site, Nav, Grid, List, Button, RouterContextProvider } from "tabler-react";
+import { Site, Nav, Grid, List, Button } from "tabler-react";
+import { getAccountDropDownMenu } from "./AccountMenu";
+import { useReduxContextValue } from "../contexts/redux-context";
 
 type Props = {
   children: React.ReactNode;
@@ -29,18 +30,6 @@ type navItem = {
   LinkComponent?: React.ElementType;
   subItems?: Array<subNavItem>;
   useExact?: boolean;
-};
-
-type accountDropdownProps = {
-  avatarURL: string;
-  name: string;
-  description: string;
-  options: {
-    icon: string;
-    value: string;
-    badge?: undefined;
-    isDivider?: undefined;
-  };
 };
 
 const withRouterNavLink = (to: string) =>
@@ -70,74 +59,69 @@ const navBarItems: Array<navItem> = [
   }
 ];
 
-const accountDropdownProps = {
-  avatarURL: userAvatar,
-  name: "Jane Pearson",
-  description: "Administrator",
-  options: [
-    { icon: "user", value: "Profile", to: "/profile" },
-    { icon: "log-out", value: "Sign out", to: "/logout" }
-  ]
-};
-
-class SiteWrapper extends React.Component<Props, State> {
-  render() {
-    return (
-      <Site.Wrapper
-        headerProps={{
-          alt: "Would You Rather?",
-          imageURL: logo,
-          navItems: (
-            <Nav.Item type="div" className="d-none d-md-flex">
-              <Button
-                href="https://github.com/cangir/udacity-reactnd-would-you-rather"
-                target="_blank"
-                outline
-                size="sm"
-                RootComponent="a"
-                color="dark"
-              >
+const SiteWrapper = (props: Props) => {
+  const { services, store } = useReduxContextValue();
+  return (
+    <Site.Wrapper
+      headerProps={{
+        alt: "Would You Rather?",
+        imageURL: logo,
+        navItems: (
+          <Nav.Item type="div" className="d-none d-md-flex">
+            <Button
+              href="https://github.com/cangir/udacity-reactnd-would-you-rather"
+              target="_blank"
+              outline
+              size="sm"
+              RootComponent="a"
+              color="dark"
+            >
+              Source code
+            </Button>
+            {/* {loginLink} */}
+          </Nav.Item>
+        ),
+        accountDropdown: getAccountDropDownMenu(services.userService, store.userState.currentUser)
+      }}
+      navProps={{ itemsObjects: navBarItems }}
+      footerProps={{
+        copyright: <React.Fragment>Copyright © 2019. All rights reserved.</React.Fragment>,
+        nav: (
+          <React.Fragment>
+            <Grid.Col auto={true}>
+              <List className="list-inline list-inline-dots mb-0">
+                <List.Item className="list-inline-item">
+                  <a href="#">Documentation</a>
+                </List.Item>
+                <List.Item className="list-inline-item">
+                  <a href="#">Issues</a>
+                </List.Item>
+              </List>
+            </Grid.Col>
+            <Grid.Col auto={true}>
+              <Button href="#" size="sm" outline color="primary" RootComponent="a">
                 Source code
               </Button>
-              {/* {loginLink} */}
-            </Nav.Item>
-          ),
-          accountDropdown: accountDropdownProps
-        }}
-        navProps={{ itemsObjects: navBarItems }}
-        footerProps={{
-          copyright: <React.Fragment>Copyright © 2019. All rights reserved.</React.Fragment>,
-          nav: (
-            <React.Fragment>
-              <Grid.Col auto={true}>
-                <List className="list-inline list-inline-dots mb-0">
-                  <List.Item className="list-inline-item">
-                    <a href="#">Documentation</a>
-                  </List.Item>
-                  <List.Item className="list-inline-item">
-                    <a href="#">Issues</a>
-                  </List.Item>
-                </List>
-              </Grid.Col>
-              <Grid.Col auto={true}>
-                <Button href="#" size="sm" outline color="primary" RootComponent="a">
-                  Source code
-                </Button>
-              </Grid.Col>
-            </React.Fragment>
-          )
-        }}
-      >
-        <div className="my-3 my-md-5">
-          <div className="container">
-            <Grid.Row>
-              <Grid.Col md={12}>{this.props.children}</Grid.Col>
-            </Grid.Row>
-          </div>
+            </Grid.Col>
+          </React.Fragment>
+        )
+      }}
+    >
+      <div className="my-3 my-md-5">
+        <div className="container">
+          <Grid.Row>
+            <Grid.Col md={12}>{props.children}</Grid.Col>
+          </Grid.Row>
         </div>
-      </Site.Wrapper>
-    );
-  }
-}
+      </div>
+    </Site.Wrapper>
+  );
+};
+
+// class SiteWrapper extends React.Component<Props, State> {
+//   render() {
+
+//   }
+// }
 
 export default SiteWrapper;
