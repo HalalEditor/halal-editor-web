a7997ad504a87ad5d3506317ba1380507e35b8f0
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-11-22 20:46:11 +0100

    feat: add authentication subscription

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,32 +1,42 @@
-import React, { FC } from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import React, { FC, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useReduxContextValue } from "./contexts/redux-context";
 import DashboardPage from "./pages/DashboardPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
 import ProfilePage from "./pages/ProfilePage";
+import { Unsubscribe } from "firebase";
 
 const Router: FC = () => {
-  const { isAuth } = useReduxContextValue().store.userState;
+  const { services, store } = useReduxContextValue();
+  const { isAuth } = store.userState;
+
+  useEffect(() => {
+    let unsubscribe: Unsubscribe;
+    setTimeout(() => {
+      unsubscribe = services.userService.subscribeAuth();
+    }, 500);
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   let pages = null;
 
   if (isAuth) {
     pages = (
-      <div className="page-container">
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/dashboard" exact component={DashboardPage} />
+        <Route path="/profile" exact component={ProfilePage} />
+        <Route path="/login" exact component={LoginPage} />
+      </Switch>
     );
   } else {
     pages = (
       <Switch>
         <Route path="/" exact component={HomePage} />
-        <Route path="/dashboard" exact component={DashboardPage} />
-        <Route path="/profile" exact component={ProfilePage} />
-        {isAuth ? <Redirect to="/" /> : <Route path="/login" exact component={LoginPage} />}
+        <Route path="/login" exact component={LoginPage} />
       </Switch>
     );
   }
