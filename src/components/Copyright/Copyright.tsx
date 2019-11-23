d7442ac42096186cc3614b64e18b7fb35565892c
cangir <ahmetcangir07@gmail.com>
2019-11-23 21:48:52 +0100

    feat: create copyright component

---
+++ src/components/Copyright/Copyright.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+import { Typography, Link } from "@material-ui/core";
+
+const Copyright: FC = () => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://www.halaleditor.com/">
+        Halal Editor
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+};
+
+export default Copyright;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
+export { default as Copyright } from "./Copyright/Copyright";
 export { default as Header } from "./Header/Header";
