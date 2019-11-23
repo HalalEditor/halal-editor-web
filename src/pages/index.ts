1875895e9972f9a0874f9292ac55b30034f82547
cangir <ahmetcangir07@gmail.com>
2019-11-23 19:53:23 +0100

    refactor: change extension of index files

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as DashboardPage } from "./Dashboard/DashboardPage";
+export { default as Error404Page } from "./Error404/Error404Page";
+export { default as HomePage } from "./Home/HomePage";
+export { default as LoginPage } from "./Login/LoginPage";
+export { default as ProfilePage } from "./Profile/ProfilePage";
+export { default as SignupPage } from "./Signup/SignupPage";
--- src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-export { DashboardPage } from "./DashboardPage";
-export { Error404Page } from "./Error404Page";
-export { HomePage } from "./HomePage";
-export { LoginPage } from "./LoginPage";
-export { ProfilePage } from "./ProfilePage";
-export { SignupPage } from "./SignupPage";
