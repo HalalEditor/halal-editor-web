e660dd1ec0750c610674361fbf14e5a8c8c84f93
cangir <ahmetcangir07@gmail.com>
2019-11-23 21:58:49 +0100

    refactor: apply material ui to login

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,15 +1,70 @@
 import React, { FC, useState } from "react";
-import Typography from "@material-ui/core/Typography";
-
 import { useReduxContextValue } from "../../contexts/redux-context";
 import { Redirect } from "react-router";
+import {
+  Avatar,
+  Button,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Box,
+  Grid,
+  Typography
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { Copyright } from "../../components";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: "100vh",
+    display: "flex"
+  },
+  paper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+    textAlign: "center"
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  },
+  margin: {
+    margin: theme.spacing(1)
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3)
+  },
+  textField: {
+    width: 200
+  }
+}));
+
+interface State {
+  amount: string;
+  password: string;
+  weight: string;
+  weightRange: string;
+  showPassword: boolean;
+}
 
 const LoginPage: FC = () => {
+  const classes = useStyles();
   const { services, store } = useReduxContextValue();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const unSubmitLoginFormHandle = async () => {
+  const onSubmitLoginFormHandle = async () => {
     console.log("loging....");
     const result = await services.userService.signInWithEmailAndPassword(email, password);
     console.log("loginButtonHandle:", result);
@@ -18,54 +73,75 @@ const LoginPage: FC = () => {
   return store.userState.isAuth ? (
     <Redirect to="/"></Redirect>
   ) : (
-    <Typography variant="h1" component="h2" gutterBottom>
-      Login Page
-    </Typography>
-    // <Page className="page-single middle">
-    //   <div className="container">
-    //     <div className="row">
-    //       <Grid.Col className="col-login mx-auto">
-    //         <div className="card">
-    //           <div className="card-body p-6">
-    //             <Button block social="google" className="text-left mb-6">
-    //               Sign in with Google
-    //             </Button>
-    //             <div className="or-seperator">
-    //               <i className="text-muted">or</i>
-    //             </div>
-    //             <Form.Group label="User Name">
-    //               <Form.Input
-    //                 icon="user"
-    //                 placeholder="E-mail address..."
-    //                 onChange={event => {
-    //                   setEmail(event.target.value);
-    //                 }}
-    //               />
-    //             </Form.Group>
-    //             <Form.Group label="Password">
-    //               <Form.Input
-    //                 type="password"
-    //                 icon="lock"
-    //                 placeholder="Password..."
-    //                 onChange={event => {
-    //                   setPassword(event.target.value);
-    //                 }}
-    //               />
-    //             </Form.Group>
-    //             <Button block color="primary" onClick={unSubmitLoginFormHandle}>
-    //               Login
-    //             </Button>
-    //           </div>
-    //         </div>
-    //         <div className="text-center">
-    //           <Text className="text-center text-muted">
-    //             Not a member? <a href="/">Sign up now</a>
-    //           </Text>
-    //         </div>
-    //       </Grid.Col>
-    //     </div>
-    //   </div>
-    // </Page>
+    <Grid container direction="row" justify="center" alignItems="center">
+      <Grid className={classes.paper} item xs={12} sm={6} md={4}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <div className={classes.form}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={event => {
+              setEmail(event.target.value);
+            }}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={event => {
+              setPassword(event.target.value);
+            }}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={onSubmitLoginFormHandle}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+          <Box mt={5}>
+            <Copyright />
+          </Box>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
