7de1bf9f0b0380bf60b95c4bff38b1bb6a152a2b
cangir <ahmetcangir07@gmail.com>
2019-11-24 03:29:17 +0100

    refactor: demo links for new pages

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,15 +1,28 @@
 import React, { FC } from "react";
-import { Typography, Button } from "@material-ui/core";
+import { Typography, Button, Grid } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
 const HomePage: FC = () => {
+  let history = useHistory();
   return (
     <React.Fragment>
       <Typography variant="h1" component="h2" gutterBottom>
         Home Page
       </Typography>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+      <Grid container justify="space-around">
+        <Button variant="contained" color="primary" onClick={() => history.push("/product")}>
+          Single Product
+        </Button>
+        <Button variant="contained" color="primary" onClick={() => history.push("/products")}>
+          Products List
+        </Button>
+        <Button variant="contained" color="primary" onClick={() => history.push("/product/add")}>
+          Add Product
+        </Button>
+        <Button variant="contained" color="primary" onClick={() => history.push("/product/edit")}>
+          Edit Product
+        </Button>
+      </Grid>
     </React.Fragment>
   );
 };
