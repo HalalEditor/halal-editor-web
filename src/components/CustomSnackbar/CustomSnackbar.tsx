4b1dfbb44db0c1bd68ef12a0af16d143acd0a05a
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2019-11-25 22:28:38 +0100

    feat: create CustomSnackbar component

---
+++ src/components/CustomSnackbar/CustomSnackbar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Snackbar } from "@material-ui/core";
+import SnackbarMessage from "./SnackbarMessage";
+import { useReduxContextValue } from "../../contexts/redux-context";
+
+const CustomSnackbar = () => {
+  const { services, store } = useReduxContextValue();
+  const { snackbarInfo } = store.appState;
+  return !snackbarInfo ? null : (
+    <Snackbar
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right"
+      }}
+      open={snackbarInfo.show}
+      autoHideDuration={6000}
+      onClose={() => services.appService.closeSnackbarMessage()}
+    >
+      <SnackbarMessage
+        onClose={() => services.appService.closeSnackbarMessage()}
+        variant={snackbarInfo.variant}
+        message={snackbarInfo.message}
+      />
+    </Snackbar>
+  );
+};
+
+export default CustomSnackbar;
