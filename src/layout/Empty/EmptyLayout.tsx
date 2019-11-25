87ac39fb8fe24466a1be5e35c251bc20feec6141
cangir <ahmetcangir07@gmail.com>
2019-11-25 21:21:15 +0100

    refactor: remove drawer form Empty Layout

--- src/layout/Empty/EmptyLayout.tsx
+++ src/layout/Empty/EmptyLayout.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { Container, Box } from "@material-ui/core";
-import { AppBar } from "../../components";
-// import { useReduxContextValue } from "../../contexts/redux-context";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const EmptyLayout = (props: Props) => {
-  // const { services, store } = useReduxContextValue();
   return (
     <React.Fragment>
       <Container>
