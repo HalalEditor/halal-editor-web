014945535cb30f5b351f84d6f703d7e4c4da4d0c
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2019-11-25 22:24:50 +0100

    feat: create app-store and add SetSnackbarMessage action to app-reducer

---
+++ src/store/app-store.ts
@@ -0,0 +1,37 @@
+import { Reducer } from "react";
+import { ActionType } from "./action-type";
+import { SnackbarInfo } from "../models/snackbar";
+
+export type AppStateType = {
+  snackbarInfo?: SnackbarInfo;
+};
+
+export type AppActionType = {
+  type: ActionType;
+  payload: ActionPayloadType;
+};
+
+export const initialAppState: AppStateType = {
+  snackbarInfo: {
+    message: "",
+    variant: "info",
+    show: false
+  }
+};
+type ActionPayloadType = { snackbarInfo?: SnackbarInfo };
+
+export const AppReducer: Reducer<AppStateType, AppActionType> = (
+  state = initialAppState,
+  action
+) => {
+  console.log(`[AppReducer]: ${JSON.stringify(action)}`);
+
+  switch (action.type) {
+    case "SetSnackbarMessage":
+      const snackbarInfo = action.payload.snackbarInfo;
+      return { ...state, snackbarInfo: snackbarInfo };
+
+    default:
+      return { ...state };
+  }
+};
