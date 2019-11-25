42c85aeb762af5515368dc21937dacf2da11d36b
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2019-11-25 22:27:43 +0100

    feat: add app-reducer and app-service to redux-context

--- src/contexts/redux-context.tsx
+++ src/contexts/redux-context.tsx
@@ -2,6 +2,8 @@ import React, { createContext, useContext, ReactChild, useReducer } from "react"
 import { ReduxValueType } from "./redux-value";
 import { UserService } from "../services/user-service";
 import { UserReducer, initialUserState } from "../store/user-store";
+import { AppReducer, initialAppState } from "../store/app-store";
+import { AppService } from "../services/app-service";
 
 export const ReduxContext = createContext<ReduxValueType>({} as ReduxValueType);
 export const useReduxContextValue = () => useContext(ReduxContext);
@@ -12,11 +14,14 @@ type Props = {
 
 const ReduxContextProvider = (props: Props) => {
   const [userState, userDispatch] = useReducer(UserReducer, initialUserState);
+  const [appState, appDispatch] = useReducer(AppReducer, initialAppState);
+
   const userService = new UserService(userDispatch);
+  const appService = new AppService(appDispatch);
 
   const appReduxValue: ReduxValueType = {
-    store: { userState },
-    services: { userService }
+    store: { userState, appState },
+    services: { userService, appService }
   };
 
   return <ReduxContext.Provider value={appReduxValue}>{props.children}</ReduxContext.Provider>;
--- src/contexts/redux-value.ts
+++ src/contexts/redux-value.ts
@@ -1,12 +1,15 @@
-import { initialUserState, UserReducer } from "../store/user-store";
+import { AppStateType } from "./../store/app-store";
+import { AppService } from "./../services/app-service";
 import { UserService } from "../services/user-service";
 import { UserStateType } from "../store/user-store";
 
 export type ReduxValueType = {
   store: {
     userState: UserStateType;
+    appState: AppStateType;
   };
   services: {
     userService: UserService;
+    appService: AppService;
   };
 };
