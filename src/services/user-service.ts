440a634d5a174c34fb748e223a604dbb3754b800
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2019-11-25 22:31:39 +0100

    refactor: use try-catch with firebase functions

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -8,44 +8,22 @@ import "firebase/auth";
 export class UserService {
   constructor(private dispatch: Dispatch<UserActionType>) {}
 
-  signInWithEmailAndPassword(email: string, password: string) {
-    console.log("signInWithEmailAndPassword", firebase);
-
-    return firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(res => {
-        const fireUser = res.user;
-        let currentUser;
-
-        if (!!fireUser) {
-          currentUser = {
-            _id: fireUser.uid,
-            email: !!fireUser.email ? fireUser.email : "",
-            emailVerified: fireUser.emailVerified,
-            userCategory: "normal"
-          } as User;
-        }
-        this.dispatch({
-          type: "SetCurrentUser",
-          payload: { user: currentUser }
-        });
-      })
-      .catch(err => {
-        return err.message;
-      });
+  async signInWithEmailAndPassword(email: string, password: string) {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      return null;
+    } catch (error) {
+      return error;
+    }
   }
 
-  createUserWithEmailAndPassword(email: string, password: string) {
-    return firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(res => {
-        console.log(`createUserWithEmailAndPassword result ${res}`);
-      })
-      .catch(err => {
-        return err.message;
-      });
+  async createUserWithEmailAndPassword(email: string, password: string) {
+    try {
+      await firebase.auth().createUserWithEmailAndPassword(email, password);
+      return null;
+    } catch (error) {
+      return error;
+    }
   }
 
   subscribeAuth = () => {
