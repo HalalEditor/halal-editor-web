0188742928ba8b33d6fde1bd505781b25b8480c3
cangir <ahmetcangir07@gmail.com>
2019-11-26 02:28:39 +0100

    refactor: remove unnecessary elements

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,31 +1,12 @@
 import React, { FC } from "react";
-import { Typography, Button, Grid } from "@material-ui/core";
-import { useHistory } from "react-router-dom";
+import { Typography } from "@material-ui/core";
 
 const HomePage: FC = () => {
-  let history = useHistory();
   return (
     <React.Fragment>
       <Typography variant="h1" component="h2" gutterBottom>
         Home Page
       </Typography>
-      <Grid container justify="space-around">
-        <Button variant="contained" color="primary" onClick={() => history.push("/login")}>
-          Login
-        </Button>
-        <Button variant="contained" color="primary" onClick={() => history.push("/product")}>
-          Single Product
-        </Button>
-        <Button variant="contained" color="primary" onClick={() => history.push("/products")}>
-          Products List
-        </Button>
-        <Button variant="contained" color="primary" onClick={() => history.push("/product/add")}>
-          Add Product
-        </Button>
-        <Button variant="contained" color="primary" onClick={() => history.push("/product/edit")}>
-          Edit Product
-        </Button>
-      </Grid>
     </React.Fragment>
   );
 };
