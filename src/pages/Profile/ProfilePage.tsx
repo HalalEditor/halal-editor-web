ea6fc203789aa2120ceba67c72f591eefc03751a
cangir <ahmetcangir07@gmail.com>
2019-11-27 02:43:27 +0100

    feat: add profile page design

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -1,8 +1,36 @@
 import React, { FC } from "react";
+import { makeStyles } from "@material-ui/styles";
+import PropTypes from "prop-types";
 import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
 
-const ProfilePage: FC = () => {
-  return <Typography variant="h3">Profile Page</Typography>;
+import { AccountProfile, AccountDetails } from "./components";
+
+type Props = {
+  className: string;
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: 4
+  }
+}));
+
+const ProfilePage = (props: Props) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={4}>
+        <Grid item lg={4} md={6} xl={4} xs={12}>
+          <AccountProfile />
+        </Grid>
+        <Grid item lg={8} md={6} xl={8} xs={12}>
+          <AccountDetails />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default ProfilePage;
