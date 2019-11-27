a412c68a439011cc6dde93c3d5c7fb6f25f9da48
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2019-11-27 10:14:11 +0100

    feat: create product store

---
+++ src/store/product-store.ts
@@ -0,0 +1,35 @@
+import { Product } from "./../models/product";
+import { Reducer } from "react";
+import { ActionType } from "./action-type";
+
+export type ProductStateType = {
+  productList: Product[];
+  favoriteProductList: Product[];
+  myProductList: Product[];
+  scannedProduct?: Product;
+  selectedProduct?: Product;
+};
+
+export type ProductActionType = {
+  type: ActionType;
+  payload: ActionPayloadType;
+};
+
+export const initialProductState: ProductStateType = {
+  productList: [],
+  favoriteProductList: [],
+  myProductList: []
+};
+type ActionPayloadType = {};
+
+export const ProductReducer: Reducer<ProductStateType, ProductActionType> = (
+  state = initialProductState,
+  action
+) => {
+  console.log(`[ProductReducer]: ${JSON.stringify(action)}`);
+
+  switch (action.type) {
+    default:
+      return { ...state };
+  }
+};
