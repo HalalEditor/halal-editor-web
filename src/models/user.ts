574a2eee6afa9866f2ea21951d461ca888a6f744
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2019-11-28 18:25:58 +0100

    feat: add fields (username and photoURL) to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,8 +1,10 @@
 export type User = {
   _id: string;
+  username: string;
   email: string;
   emailVerified: boolean;
-  userCategory?: UserCategory;
+  photoURL: string;
+  userCategory: UserCategory;
 };
 
 export type UserCategory = "root" | "admin" | "editor" | "normal";
