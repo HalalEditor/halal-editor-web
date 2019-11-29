3d3657694eac09a726825785b72f0b6b0a1660ef
cangir <ahmetcangir07@gmail.com>
2019-11-29 17:13:32 +0100

    refactor: temporary redirect home to dashboard

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,8 +1,13 @@
 import React, { FC } from "react";
 import { Typography } from "@material-ui/core";
+import { useReduxContextValue } from "../../contexts/redux-context";
+import { Redirect } from "react-router";
 
 const HomePage: FC = () => {
-  return (
+  const { store } = useReduxContextValue();
+  return store.userState.isAuth ? (
+    <Redirect to="/dashboard"></Redirect>
+  ) : (
     <React.Fragment>
       <Typography variant="h3">Home Page</Typography>
     </React.Fragment>
