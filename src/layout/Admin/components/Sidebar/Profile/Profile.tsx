f84e4fca4f88e0ef1f28fbe8049e3bd8c9e9a225
cangir <ahmetcangir07@gmail.com>
2019-12-02 16:55:54 +0100

    refactor: get current user from store

--- src/layout/Admin/components/Sidebar/Profile/Profile.tsx
+++ src/layout/Admin/components/Sidebar/Profile/Profile.tsx
@@ -8,26 +8,29 @@ const Profile = () => {
   const classes = useStyles();
   const { store } = useReduxContextValue();
 
+  const currentUser = store.userState.currentUser;
   const user = {
-    name: "Ahmet Cangir",
-    avatar: "http://localhost:3000/assets/user.svg",
-    email: "ahmetcangir07@gmail.com"
+    name: !!currentUser ? currentUser.username : "",
+    avatar: !!currentUser ? currentUser.photoURL : "",
+    email: !!currentUser ? currentUser.email : ""
   };
 
   return (
-    <div className={classes.root}>
-      <Avatar
-        alt="Person"
-        className={classes.avatar}
-        component={RouterLink}
-        src={user.avatar}
-        to="/profile"
-      />
-      <Typography className={classes.name} variant="h4">
-        {user.name}
-      </Typography>
-      <Typography variant="body2">{user.email}</Typography>
-    </div>
+    <React.Fragment>
+      <div className={classes.root}>
+        <Avatar
+          alt="Person"
+          className={classes.avatar}
+          component={RouterLink}
+          src={user.avatar}
+          to="/profile"
+        />
+        <Typography className={classes.name} variant="h4">
+          {user.name}
+        </Typography>
+        <Typography variant="body2">{user.email}</Typography>
+      </div>
+    </React.Fragment>
   );
 };
 
