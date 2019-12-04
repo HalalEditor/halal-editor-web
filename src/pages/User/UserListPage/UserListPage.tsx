4830419ce415b57fa60c4437b49cee291aec6d05
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2019-12-04 14:19:08 +0100

    feat: implement userCategory filter methods to UserListPage

--- src/pages/User/UserListPage/UserListPage.tsx
+++ src/pages/User/UserListPage/UserListPage.tsx
@@ -8,25 +8,39 @@ import useInfiniteScroll from "../../../hooks/useInfiniteScroll";
 
 import ToggleButton from "@material-ui/lab/ToggleButton";
 import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+import { UserCategory } from "../../../models/user";
 
-const LOAD_LIMIT = 10;
+const LOAD_LIMIT = 20;
 
 const UserListPage = () => {
+  type SelectedUserCategoryType = UserCategory | "all";
   const classes = useStyles();
   const { store, services } = useReduxContextValue();
   const { userList } = store.userState;
-  const [userCount, setUserCount] = useState(0);
+  const [userCount, setUserCount] = useState({ all: 0, admin: 0, editor: 0 });
   const { isFetching, setIsFetching } = useInfiniteScroll(loadMoreUser);
 
-  const [loadPage, setLoadPage] = useState({ limit: LOAD_LIMIT, searchKey: "" });
+  const [loadPage, setLoadPage] = useState<{
+    limit: number;
+    searchKey: string;
+    selectedUserCategory: SelectedUserCategoryType;
+  }>({
+    limit: LOAD_LIMIT,
+    searchKey: "",
+    selectedUserCategory: "all"
+  });
 
   const { userService } = services;
   const { userState } = store;
 
   useEffect(
     () => {
-      userService.getUserCount().then(count => {
-        setUserCount(count);
+      Promise.all([
+        userService.getUserCount(),
+        userService.getUserCount("admin"),
+        userService.getUserCount("editor")
+      ]).then(res => {
+        setUserCount({ all: res[0], admin: res[1], editor: res[2] });
       });
     },
     // eslint-disable-next-line
@@ -37,39 +51,57 @@ const UserListPage = () => {
     () => {
       const lastLoadUserId = userList.length > 0 ? userList[userList.length - 1]._id : undefined;
 
-      userService.loadUserList(loadPage.limit, lastLoadUserId, loadPage.searchKey).then(_ => {
-        setIsFetching(false);
-      });
+      userService
+        .loadUserList({
+          limit: loadPage.limit,
+          lastUserId: lastLoadUserId,
+          searchingMail: loadPage.searchKey,
+          selectedUserCategory:
+            loadPage.selectedUserCategory === "all" ? undefined : loadPage.selectedUserCategory
+        })
+        .then(_ => {
+          setIsFetching(false);
+        });
     },
     // eslint-disable-next-line
     [loadPage]
   );
 
   function loadMoreUser() {
-    if (userCount > userState.userList.length) {
-      setLoadPage({ ...loadPage });
-    } else {
-      setIsFetching(false);
+    if (loadPage.selectedUserCategory !== "root" && loadPage.selectedUserCategory !== "normal") {
+      let selectedGroupUserCount = userCount[loadPage.selectedUserCategory];
+      if (selectedGroupUserCount > userState.userList.length) {
+        setLoadPage({ ...loadPage });
+      } else {
+        setIsFetching(false);
+      }
     }
   }
 
   const handleSearchEvent = (searchKey: string) => {
-    setLoadPage({ searchKey: searchKey, limit: LOAD_LIMIT });
+    userService.clearUserList();
+    setLoadPage({
+      searchKey: searchKey,
+      limit: LOAD_LIMIT,
+      selectedUserCategory: loadPage.selectedUserCategory
+    });
   };
 
   let users = userState.userList.map(user => {
     // TODO: add property (user-category, email-verified) to user card
-    // TODO: connect user image
-    // TODO: users will be filtered with userCategory state
     return <UserItem key={user._id} user={user} onClick={() => console.log(user._id)} />;
   });
   for (let i = 0; i < 10; i++) {
     users.push();
   }
 
-  const [userCategory, setUserCategory] = React.useState("all");
-  const handleUserCategory = (event: React.MouseEvent<HTMLElement>, newUserCategory: string) => {
-    setUserCategory(newUserCategory);
+  const handleSelectedUserCategory = (
+    event: React.MouseEvent<HTMLElement>,
+    selectedUserCategory: SelectedUserCategoryType
+  ) => {
+    console.log(selectedUserCategory);
+    userService.clearUserList();
+    setLoadPage({ ...loadPage, selectedUserCategory: selectedUserCategory });
   };
 
   return (
@@ -80,18 +112,18 @@ const UserListPage = () => {
         <div className={classes.toggleContainer}>
           <ToggleButtonGroup
             size="small"
-            value={userCategory}
+            value={loadPage.selectedUserCategory}
             exclusive
-            onChange={handleUserCategory}
+            onChange={handleSelectedUserCategory}
           >
             <ToggleButton value="all" aria-label="All Users">
-              <span className={classes.toggleButtonText}>All (99)</span>
+              <span className={classes.toggleButtonText}>All ({userCount.all})</span>
             </ToggleButton>
             <ToggleButton value="admin" aria-label="Admins">
-              <span className={classes.toggleButtonText}>Admins (3)</span>
+              <span className={classes.toggleButtonText}>Admins ({userCount.admin})</span>
             </ToggleButton>
             <ToggleButton value="editor" aria-label="Editors">
-              <span className={classes.toggleButtonText}>Editors (5)</span>
+              <span className={classes.toggleButtonText}>Editors ({userCount.editor})</span>
             </ToggleButton>
           </ToggleButtonGroup>
         </div>
