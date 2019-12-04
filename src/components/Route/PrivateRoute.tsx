0c440bfd9cbdc4150a35d4cd716244b8dfcea08a
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2019-12-04 14:51:53 +0100

    fix: currentUser load late

--- src/components/Route/PrivateRoute.tsx
+++ src/components/Route/PrivateRoute.tsx
@@ -1,28 +1,29 @@
 import * as React from "react";
 import { Route as ReactRoute, Redirect } from "react-router-dom";
 import { useReduxContextValue } from "../../contexts/redux-context";
+import { UserCategory } from "../../models/user";
 
 interface Props {
   component: any;
   layout: any;
   path: string;
   exact?: boolean;
-  group?: string[];
+  accessibleUserCategories?: UserCategory[];
 }
 
-const PrivateRoute = ({ component, layout, path, exact, group }: Props) => {
+const PrivateRoute = ({ component, layout, path, exact, accessibleUserCategories: auc }: Props) => {
   const { store, services } = useReduxContextValue();
-  const { currentUser } = store.userState;
-
   const localUser = services.userService.getLocalUser();
-  const isAuth = !!currentUser || !!localUser;
-  const hasPower = !!currentUser && !!group && group.indexOf(currentUser.userCategory) > -1;
+  const currentUser = store.userState.currentUser || localUser;
+
+  const isAuth = !!currentUser;
+  const canAccess = !!currentUser && !!auc && auc.indexOf(currentUser.userCategory) !== -1;
 
   const Layout = layout;
   const Component = component;
   return !isAuth ? (
     <Redirect to="/login" />
-  ) : !!group && !hasPower ? (
+  ) : !canAccess ? (
     <Redirect to="/admin404" />
   ) : (
     <ReactRoute
