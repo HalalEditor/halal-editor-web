0eca8802a8c9b3a0f46457863bc20db8aa126d6c
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2019-12-08 18:55:39 +0100

    feat: create padLeft function

--- src/services/helper.ts
+++ src/services/helper.ts
@@ -15,3 +15,7 @@ export const delay = (millisecond: number) => {
     setTimeout(resolve, millisecond);
   });
 };
+
+export const padLeft = (text: string, padChar: string, size: number): string => {
+  return (String(padChar).repeat(size) + text).substr(size * -1, size);
+};
