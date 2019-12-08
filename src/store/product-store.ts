ffb26af5fbda9093b1d3816e38e0a5479e61092b
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2019-12-08 20:11:36 +0100

    feat: implement arrayToDictionary to product-reducer

--- src/store/product-store.ts
+++ src/store/product-store.ts
@@ -1,6 +1,7 @@
 import { Reducer } from "react";
 import { ActionType } from "./action-type";
 import { ProductDTO, ProductDictionary } from "../dto/product-dto";
+import { arrayToDictionary } from "./../services/helper";
 
 export type ProductStateType = {
   productList: ProductDictionary;
@@ -36,14 +37,7 @@ export const ProductReducer: Reducer<ProductStateType, ProductActionType> = (
   switch (action.type) {
     case "AddProductList":
       if (!!action.payload.productList) {
-        const addProductList = action.payload.productList.reduce(
-          (list: ProductDictionary, product) => {
-            list[product.mainInfo._id] = product;
-            return list;
-          },
-          {}
-        );
-
+        const addProductList = arrayToDictionary(action.payload.productList, "_id");
         return { ...state, productList: { ...state.productList, ...addProductList } };
       } else return { ...state };
 
@@ -52,13 +46,7 @@ export const ProductReducer: Reducer<ProductStateType, ProductActionType> = (
 
     case "SetFavoriteProductList":
       if (!!action.payload.favoriteProductList) {
-        const favoriteProductList = action.payload.favoriteProductList.reduce(
-          (list: ProductDictionary, product) => {
-            list[product.mainInfo._id] = product;
-            return list;
-          },
-          state.favoriteProductList
-        );
+        const favoriteProductList = arrayToDictionary(action.payload.favoriteProductList, "_id");
 
         return { ...state, favoriteProductList: { ...favoriteProductList } };
       } else return { ...state };
