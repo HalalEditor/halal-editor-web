03add47a9ee213d2b1238db7e05b8cb30cbdc807
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2019-12-09 20:29:21 +0100

    feat: create updateProductLikes function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,32 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.updateProductLikes = functions.firestore
+  .document("/users/{userId}/fav-products/{productId}")
+  .onWrite(async (change, context) => {
+    const { productId, userId } = context.params;
+    const productRef = db.collection("products").doc(productId);
+    const likesRef = productRef.collection("likes");
+    const user = await db.doc(`/users/${userId}`).get();
+
+    if (user.exists) {
+      if (!change.before.exists && change.after.exists) {
+        await likesRef.doc(userId).set({ email: (user.data() as any).email });
+      } else {
+        await likesRef.doc(userId).delete();
+      }
+
+      return db.runTransaction(transaction => {
+        return transaction.get(likesRef).then(likesQuery => {
+          const likesCount = likesQuery.size;
+
+          return transaction.update(productRef, {
+            likesCount: likesCount
+          });
+        });
+      });
+    } else return;
+  });
