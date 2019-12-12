f7ef93cdad24b77374ef68efcfcaa303f6cb3aa0
cangir <ahmetcangir07@gmail.com>
2019-12-12 17:13:39 +0100

    refactor: remove old Error page

--- src/pages/Error/Error404Page.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-import Typography from "@material-ui/core/Typography";
-
-const Error404Page: FC = () => {
-  return <Typography variant="h3">Error 404 Page</Typography>;
-};
-
-export default Error404Page;
