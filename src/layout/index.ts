51c347067b80e1657715e68f880e2459d6432329
cangir <ahmetcangir07@gmail.com>
2019-12-12 17:21:57 +0100

    refactor: add Error layout to layout index

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,3 +1,4 @@
 export { default as AdminLayout } from "./Admin/AdminLayout";
 export { default as DefaultLayout } from "./Default/DefaultLayout";
 export { default as EmptyLayout } from "./Empty/EmptyLayout";
+export { default as ErrorLayout } from "./Error/ErrorLayout";
